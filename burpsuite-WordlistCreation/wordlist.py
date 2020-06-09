3db96e3cd5195625b6f0413db845463fed1c961f
Fib <jshaferfam@protonmail.com>
2020-06-09 09:00:44 -0500

    Update wordlist.py

--- burpsuite-WordlistCreation/wordlist.py
+++ burpsuite-WordlistCreation/wordlist.py
@@ -8,10 +8,8 @@ def stripIt(unmodified):
     removeHTTP = unmodified.split("/")[3:]  #strip from 3rd '/' onward with /'s eg. http://abc.com/ <-
     return removeHTTP
 
-def findSubDirs():
-    inFile = sys.argv[1]    #get our unedited list of urls
-    with open(inFile, 'r') as i:    #create a new list of all these urls
-        lines = i.readlines()
+def findSubDirs(lines):
+   
     
     for i in range(len(lines)):
         lines[i] = stripIt(lines[i]) #strip out directories
@@ -22,12 +20,33 @@ def findSubDirs():
             separatedListUnsorted.append(text)
     
     separatedList = uniqthis(separatedListUnsorted)
-    outFile = sys.argv[2]
+    outFile = sys.argv[2] + "-Subdirectories"
 
     with open(outFile, 'w') as out:
         for line in separatedList:
             if '&' in line or '?' in line or ':' in line or '.' in line:    #only want directories, not filenames or params
                 pass
+            else:
+                out.write(line + "\n")
+
+def findFileNames(lines):
+    fileList = []
+    for i in lines:
+        fileName = i
+        firstPos = i.rfind("/")
+        lastPos = len(i)
+        testFile = i[firstPos+1:lastPos]
+        if "." in testFile:
+            if '&' in testFile or '?' in testFile:
+                pass
+            else:
+                fileList.append(testFile)
+    outFile = sys.argv[2] + "-fileNames"
+    uniqthis(fileList) #not working for some reason yet
+    with open(outFile, 'w') as out:
+        for line in fileList:
+            if "\n" in line:    #not sure why, some contain newlines.. some don't.. janky workaround
+                out.write(line)
             else:
                 out.write(line + "\n")
 
@@ -37,7 +56,13 @@ def startup():
     elif len(sys.argv) == 2:
         print("must provide output filename")
     elif len(sys.argv) == 3:
-        findSubDirs()
+        inFile = sys.argv[1]    #get our unedited list of urls
+        with open(inFile, 'r') as i:    #create a new list of all these urls
+            lines = i.readlines()
+        with open(inFile, 'r') as i:
+            lines2 = i.readlines()
+        findSubDirs(lines)
+        findFileNames(lines2)
     else:
         sys.exit()
 
